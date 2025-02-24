735b8cfb5dec3c84d261de43033aa864a82210e3
mhgarry <mhgarry92@gmail.com>
2025-02-24 15:59:56 -0500

    make random number functional

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,16 +9,16 @@ use rand::Rng;
 use std::io;
 
 fn main() {
-    println!("Guess a number between 1 and 100!");
-    println!("Please enter your guess:");
+    println!("Guess a number between 1 and 100!"); // print a message to the user.
+
+    let secret_number = rand::thread_rng().gen_range(1..=100); // generate a random number between 1 and 100.
+
+    println!("The secret number is: {secret_number}"); // print the secret number for debugging purposes.
+
+    println!("Please enter your guess:"); // prompt the user to enter a guess.
 
     // mut indicates a mutable variable in Rust (default is immutable) for dynamic user input.
     let mut guess = String::new();
-
-    // generate a random secret number between 1 and 100 that the user will try to guess.
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    // print the secret number to console for testing
-    println!("The secret number is: {}", secret_number);
 
     io::stdin()
         .read_line(&mut guess) // read_line takes the user input and stores it in the guess variable.
