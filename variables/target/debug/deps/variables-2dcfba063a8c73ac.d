41ab3bbd829567d9c09987f7f7530640f57a08e3
mhgarry <mhgarry92@gmail.com>
2025-02-24 23:44:56 -0500

    example of a mutable variable

---
+++ variables/target/debug/deps/variables-2dcfba063a8c73ac.d
@@ -0,0 +1,5 @@
+/Users/matthewgarry/rust_starter_projects/variables/target/debug/deps/libvariables-2dcfba063a8c73ac.rmeta: src/main.rs
+
+/Users/matthewgarry/rust_starter_projects/variables/target/debug/deps/variables-2dcfba063a8c73ac.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/deps/variables-b920e5b645c68003.d
@@ -0,0 +1,5 @@
+/Users/matthewgarry/rust_starter_projects/variables/target/debug/deps/libvariables-b920e5b645c68003.rmeta: src/main.rs
+
+/Users/matthewgarry/rust_starter_projects/variables/target/debug/deps/variables-b920e5b645c68003.d: src/main.rs
+
+src/main.rs:
